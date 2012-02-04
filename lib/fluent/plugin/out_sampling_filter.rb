e1748eaaad8d147522b6bb6d0f49c86071755f6a
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-02-04 15:31:04 +0900

    write code and its test

---
+++ lib/fluent/plugin/out_sampling_filter.rb
@@ -0,0 +1,64 @@
+class Fluent::SamplingFilterOutput < Fluent::Output
+  Fluent::Plugin.register_output('sampling_filter', self)
+
+  config_param :interval, :integer
+  config_param :sample_unit, :string, :default => 'tag'
+  config_param :remove_prefix, :string, :default => nil
+  config_param :add_prefix, :string, :default => 'sampled'
+
+  def configure(conf)
+    super
+
+    if @remove_prefix
+      @removed_prefix_string = @remove_prefix + '.'
+      @removed_length = @removed_prefix_string.length
+    end
+    @added_prefix_string = @add_prefix + '.'
+
+    @sample_unit = case @sample_unit
+                   when 'tag'
+                     :tag
+                   when 'all'
+                     :all
+                   else
+                     raise Fluent::ConfigError, "sample_unit allows only 'tag' or 'all'"
+                   end
+    @counts = {}
+  end
+
+  def emit_sampled(tag, time_record_pairs)
+    if @remove_prefix and
+        ( (tag.start_with?(@removed_prefix_string) and tag.length > @removed_length) or tag == @remove_prefix)
+      tag = tag[@removed_length..-1]
+    end
+    tag = if tag.length > 0
+            @added_prefix_string + tag
+          else
+            @add_prefix
+          end
+
+    time_record_pairs.each {|t,r|
+      Fluent::Engine.emit(tag, t, r)
+    }
+  end
+
+  def emit(tag, es, chain)
+    t = if @sample_unit == :all
+          'all'
+        else
+          tag
+        end
+    @counts[t] ||= 0
+    pairs = []
+    es.each {|time,record|
+      @counts[t] += 1
+      if @counts[t] == @interval
+        pairs.push [time, record]
+        @counts[t] = 0
+      end
+    }
+    emit_sampled(tag, pairs)
+
+    chain.next
+  end
+end
--- test/plugin/test_out_sampling_filter.rb
+++ test/plugin/test_out_sampling_filter.rb
@@ -1,7 +1,100 @@
 require 'helper'
 
-class TestFluentPluginSamplingFilter < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class SamplingFilterOutputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+  end
+  
+  CONFIG = %[
+    interval 10
+    sample_unit tag
+    remove_prefix input
+    add_prefix sampled
+  ]
+
+  def create_driver(conf=CONFIG,tag='test')
+    Fluent::Test::OutputTestDriver.new(Fluent::SamplingFilterOutput, tag).configure(conf)
+  end
+
+  def test_configure
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver('')
+    }
+    d = create_driver %[
+      interval 5
+    ]
+
+    assert_equal 5, d.instance.interval
+    assert_equal :tag, d.instance.sample_unit
+    assert_nil d.instance.remove_prefix
+    assert_equal 'sampled', d.instance.add_prefix
+
+    d = create_driver %[
+      interval 1000
+      sample_unit all
+      remove_prefix test
+      add_prefix output      
+    ]
+    assert_equal 1000, d.instance.interval
+    assert_equal :all, d.instance.sample_unit
+    assert_equal 'test', d.instance.remove_prefix
+    assert_equal 'output', d.instance.add_prefix
+  end
+
+  # CONFIG = %[
+  #   interval 10
+  #   sample_unit tag
+  #   remove_prefix input
+  #   add_prefix sampled
+  # ]
+  def test_emit
+    d1 = create_driver(CONFIG, 'input.hoge1')
+    time = Time.parse("2012-01-02 13:14:15").to_i
+    d1.run do
+      d1.emit({'field1' => 'record1', 'field2' => 1})
+      d1.emit({'field1' => 'record2', 'field2' => 2})
+      d1.emit({'field1' => 'record3', 'field2' => 3})
+      d1.emit({'field1' => 'record4', 'field2' => 4})
+      d1.emit({'field1' => 'record5', 'field2' => 5})
+      d1.emit({'field1' => 'record6', 'field2' => 6})
+      d1.emit({'field1' => 'record7', 'field2' => 7})
+      d1.emit({'field1' => 'record8', 'field2' => 8})
+      d1.emit({'field1' => 'record9', 'field2' => 9})
+      d1.emit({'field1' => 'record10', 'field2' => 10})
+      d1.emit({'field1' => 'record11', 'field2' => 11})
+      d1.emit({'field1' => 'record12', 'field2' => 12})
+    end
+    emits = d1.emits
+    assert_equal 1, emits.length
+    assert_equal 'sampled.hoge1', emits[0][0] # tag
+    assert_equal 'record10', emits[0][2]['field1']
+    assert_equal 10, emits[0][2]['field2']
+
+    d2 = create_driver(%[
+      interval 3
+    ], 'input.hoge2')
+    time = Time.parse("2012-01-02 13:14:15").to_i
+    d2.run do
+      d2.emit({'field1' => 'record1', 'field2' => 1})
+      d2.emit({'field1' => 'record2', 'field2' => 2})
+      d2.emit({'field1' => 'record3', 'field2' => 3})
+      d2.emit({'field1' => 'record4', 'field2' => 4})
+      d2.emit({'field1' => 'record5', 'field2' => 5})
+      d2.emit({'field1' => 'record6', 'field2' => 6})
+      d2.emit({'field1' => 'record7', 'field2' => 7})
+      d2.emit({'field1' => 'record8', 'field2' => 8})
+      d2.emit({'field1' => 'record9', 'field2' => 9})
+      d2.emit({'field1' => 'record10', 'field2' => 10})
+      d2.emit({'field1' => 'record11', 'field2' => 11})
+      d2.emit({'field1' => 'record12', 'field2' => 12})
+    end
+    emits = d2.emits
+    assert_equal 4, emits.length
+    assert_equal 'sampled.input.hoge2', emits[0][0] # tag
+
+    assert_equal 'record3', emits[0][2]['field1']
+    assert_equal 'record6', emits[1][2]['field1']
+    assert_equal 'record9', emits[2][2]['field1']
+    assert_equal 'record12', emits[3][2]['field1']
   end
 end
