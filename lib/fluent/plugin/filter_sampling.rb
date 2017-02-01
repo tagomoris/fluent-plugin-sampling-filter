f61f7e47f110a41cebe740a453a990a2bb6f8907
TAGOMORI Satoshi <tagomoris@gmail.com>
2017-02-01 13:49:26 +0900

    update filter implementation with enum type and lock, without #filter_stream

--- lib/fluent/plugin/filter_sampling.rb
+++ lib/fluent/plugin/filter_sampling.rb
@@ -1,64 +1,56 @@
 require 'fluent/plugin/filter'
+require 'fluent/clock'
 
 class Fluent::Plugin::SamplingFilter < Fluent::Plugin::Filter
   Fluent::Plugin.register_filter('sampling_filter', self)
 
   config_param :interval, :integer
-  config_param :sample_unit, :string, default: 'tag'
+  config_param :sample_unit, :enum, list: [:tag, :all], default: :tag
   config_param :minimum_rate_per_min, :integer, default: nil
 
   def configure(conf)
     super
 
-    @sample_unit = case @sample_unit
-                   when 'tag'
-                     :tag
-                   when 'all'
-                     :all
-                   else
-                     raise Fluent::ConfigError, "sample_unit allows only 'tag' or 'all'"
-                   end
     @counts = {}
     @resets = {} if @minimum_rate_per_min
   end
 
-  def filter_stream(tag, es)
-    t = if @sample_unit == :all
-          'all'
-        else
-          tag
-        end
+  # Access to @counts SHOULD be protected by mutex, with a heavy penalty.
+  # Code below is not thread safe, but @counts (counter for sampling rate) is not
+  # so serious value (and probably will not be broken...),
+  # then i let here as it is now.
 
-    new_es = Fluent::MultiEventStream.new
-
-    # Access to @counts SHOULD be protected by mutex, with a heavy penalty.
-    # Code below is not thread safe, but @counts (counter for sampling rate) is not
-    # so serious value (and probably will not be broken...),
-    # then i let here as it is now.
+  def filter(tag, _time, record)
+    t = @sample_unit == :all ? 'all' : tag
     if @minimum_rate_per_min
-      unless @resets[t]
-        @resets[t] = Fluent::Engine.now + (60 - rand(30))
-      end
-      if Fluent::Engine.now > @resets[t]
-        @resets[t] = Fluent::Engine.now + 60
-        @counts[t] = 0
-      end
-      es.each do |time,record|
-        c = (@counts[t] = @counts.fetch(t, 0) + 1)
-        if c < @minimum_rate_per_min or c % @interval == 0
-          new_es.add(time, record.dup)
-        end
-      end
+      filter_with_minimum_rate(t, record)
     else
-      es.each do |time,record|
-        c = (@counts[t] = @counts.fetch(t, 0) + 1)
-        if c % @interval == 0
-          new_es.add(time, record.dup)
-          # reset only just before @counts[t] is to be bignum from fixnum
-          @counts[t] = 0 if c > 0x6fffffff
-        end
-      end
+      filter_simple(t, record)
     end
-    new_es
+  end
+
+  def filter_simple(t, record)
+    c = (@counts[t] = @counts.fetch(t, 0) + 1)
+    # reset only just before @counts[t] is to be bignum from fixnum
+    @counts[t] = 0 if c > 0x6fffffff
+    if c % @interval == 0
+      record
+    else
+      nil
+    end
+  end
+
+  def filter_with_minimum_rate(t, record)
+    @resets[t] ||= Fluent::Clock.now + (60 - rand(30))
+    if Fluent::Clock.now > @resets[t]
+      @resets[t] = Fluent::Clock.now + 60
+      @counts[t] = 0
+    end
+    c = (@counts[t] = @counts.fetch(t, 0) + 1)
+    if c < @minimum_rate_per_min || c % @interval == 0
+      record.dup
+    else
+      nil
+    end
   end
 end
